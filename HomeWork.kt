16b1ba4971288c3eb6393a3eda0ced2d333d1f22
ankit <you@example.com>
2023-04-15 18:01:16 +0530

    commit

---
+++ HomeWork.kt
@@ -0,0 +1,8 @@
+fun main(){
+    println("Enter your name: " )
+    val name = readlnOrNull()
+    println("Enter your age: ")
+    val age = readlnOrNull()
+    println("Your name is $name\nyour age is : $age")
+
+}
--- Tutorials.kt
+++ Tutorials.kt
@@ -1,8 +1,29 @@
+import org.jetbrains.annotations.TestOnly
 
+fun main() {
+    // variable // variable name // Added colon to set the type(:)
+//    var myVariable: Int = 5
+//    println(myVariable)
+//    myVariable = 4
+//    println("My variable value is : $myVariable")
 
-fun main(){
-
-    println("Hello")
-
+    // Boolean
+//    var myVariable: Boolean = true
+//    println(myVariable)
+//    myVariable = false
+//    println(myVariable)
 }
+
+// Create a function to print the value of the variable
+//fun printVariable() {
+//    println(myVariable)
+//}
+
+// Create a function to print the value of the variable
+
+//fun printVariable() {
+//    println(myVariable)
+//}
+
+
 
