b7fd530c35f17306e558185c5ccff27e351d9e4f
ankit <you@example.com>
2023-06-03 02:05:33 +0530

    commit"Arithmetic"

--- HomeWork.kt
+++ HomeWork.kt
@@ -1,8 +1,19 @@
-fun main(){
-    println("Enter your name: " )
-    val name = readlnOrNull()
-    println("Enter your age: ")
-    val age = readlnOrNull()
-    println("Your name is $name\nyour age is : $age")
+//fun main(){
+//    println("Enter your name: " )
+//    val name = readlnOrNull()
+//    println("Enter your age: ")
+//    val age = readlnOrNull()
+//    println("Your name is $name\nyour age is : $age")}
 
+fun main() {
+    arithmetic()
+}
+
+fun arithmetic() {
+    val x = 3
+    println("x: $x")
+    val y = 4
+    println("x+y: ${x + y}")
+    val z = 5
+    println("x+y+z: ${x + y + z}")
 }
