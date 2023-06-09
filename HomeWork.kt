ffb883fb9791aa5b8f83a579f490284bf227fbf9
ankit <you@example.com>
2023-06-09 08:44:06 +0530

    commit"Arithmetic"

--- HomeWork.kt
+++ HomeWork.kt
@@ -1,19 +1,51 @@
-//fun main(){
-//    println("Enter your name: " )
-//    val name = readlnOrNull()
-//    println("Enter your age: ")
-//    val age = readlnOrNull()
-//    println("Your name is $name\nyour age is : $age")}
-
 fun main() {
-    arithmetic()
+
 }
+
+/*
+fun name(){
+    println("Enter your name: " )
+    val name = readlnOrNull()
+    println("Enter your age: ")
+    val age = readlnOrNull()
+    println("Your name is $name\n your age is : $age")}
 
 fun arithmetic() {
-    val x = 3
+    val x: Double = 305.22
     println("x: $x")
-    val y = 4
-    println("x+y: ${x + y}")
-    val z = 5
-    println("x+y+z: ${x + y + z}")
+    val y: Int = 4
+    println("x+y: ${x + y.toDouble()}")
+    val z: Int = 5
+    println("x+y+z: ${x + y.toDouble() + z.toDouble()}")
 }
+fun volume(){
+    val pi = 3.14
+    val radius = 5.5 * 5.5 * 5.5
+    println("The volume of the sphere with the radius 5.5 is ${4/3*3.14*5.5*5.5*5.5}")
+}
+
+fun string(
+    name: String
+) {
+    println("Name is :{${name.uppercase().reversed()}}")
+}
+
+fun simpleExpression() {
+    val simpleExpression = 3 > 4 || 4 > 3 && 4 <= 4
+    println(simpleExpression)
+}
+fun hardExpression() {
+     val bool = true
+    val x = 9
+    val y = 3
+    val z = 9
+    val hardExpression = !(x != z) && bool || z > (x + y) && (!bool || y < z)
+                          // True && True || False  && (true)
+                            // True || False
+                                // OR
+                                // TRUE
+    println(hardExpression)
+}
+*/
+
+
